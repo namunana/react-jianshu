6ae48f4cd8669b4d7780e160c2c52741972458ff
namunana <2656642905@qq.com>
2022-06-11 12:27:21 +0800

    路由的使用

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from "react";
 import Header from "./common/header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from "react-redux";
 import store from "./store";
+import Detail from "./pages/detail";
+import Home from "./pages/home";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" exact element={<Home />}></Route>
+              <Route path="/detail" exact element={<Detail />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
       </Provider>
     )
   }
